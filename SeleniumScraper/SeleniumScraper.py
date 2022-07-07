d506dbe1cf24fc5f76c306b9dbf3b548e08474b2
Nathan Spencer <nathanspencer97@hotmail.com>
2022-07-06 22:32:43 -0400

    first commit

---
+++ SeleniumScraper/SeleniumScraper.py
@@ -0,0 +1,55 @@
+# webdriver main functionality
+from selenium import webdriver # pip install selenium
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager # pip install webdriver-manager
+
+# additional imports
+from bs4 import BeautifulSoup # pip install bs4
+
+
+# Enables scraping of dynamic websites using selenium
+#
+class SeleniumScraper:
+    def __init__(self) -> None:
+        self.driver = None
+        self.__load_strategy = "normal" # accepts "none" and "eager"
+        self.__webdriver_data_folder = "webdriver_folder"
+        self.__options = Options()
+        self.__initialize_driver()
+
+
+    # Set the basic webdriver options
+    #
+    def __initialize_driver(self) -> None:
+        # set the options
+        self.__options.page_load_strategy = self.__load_strategy
+        self.__options.add_argument("user-data-dir={}".format(self.__webdriver_data_folder))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.__options)
+        
+        
+    # Use the webdriver to get the specified url
+    #
+    def get(self, url:str) -> None:
+        self.driver.get(url)
+        
+    
+    # Return a BeautifulSoup object of the entire page source from the driver.
+    # Call get() before using this method
+    #
+    def get_soup(self) -> BeautifulSoup:
+        soup = BeautifulSoup(self.driver.page_source, features='html.parser')
+        return soup
+
+
+# client test code
+if __name__ == "__main__":
+    driver = SeleniumScraper()
+    driver.get("https://pypi.org/project/selenium/")
+    soup = driver.get_soup()
+    header = soup.find("h1", {"class":"package-header__name"}).text
+    if "selenium" in header:
+        print("Success")
+    else:
+        print("Failure")
+    
---
+++ apps/youtube_explorer.py
@@ -0,0 +1,57 @@
+from selenium.webdriver.common.by import By
+import time
+import sys
+
+# import the project
+sys.path.insert(0, "SeleniumScraper")
+from SeleniumScraper import SeleniumScraper
+
+
+# client code to get all the videos from the youtube explore feed
+if __name__ == "__main__":
+    # set up the scraper
+    driver = SeleniumScraper()
+    
+    # request the youtube explore feed
+    driver.get("https://www.youtube.com/feed/explore")
+    
+    # get the soup from the library
+    soup = driver.get_soup()
+    
+    # test to make sure the header isnt a cookie test...
+    h1 = soup.find("h1")
+    if h1 is not None:
+        text = h1.text
+        if text == "Before you continue to YouTube":
+            reject_element = driver.driver.find_element(By.CSS_SELECTOR, "button[jsname='tWT92d']")
+            reject_element.click()
+            time.sleep(1)
+            soup = driver.get_soup()
+            
+    # use the soup to parse each video
+    videos = soup.find_all("ytd-video-renderer", {"class":"style-scope ytd-expanded-shelf-contents-renderer"})
+    for video in videos:
+        # get the video main info
+        title_element = video.find("a", {"id":"video-title"})
+        link = title_element.attrs['href']
+        title = title_element.text
+        
+        # get the video meta info
+        meta_line = video.find("div", {"id":"metadata-line"})
+        meta_elements = meta_line.find_all("span")
+        views = meta_elements[0].text
+        posted_time = meta_elements[1].text
+        
+        # get the channel info
+        channel_element = video.find("div", {"id":"byline-container"}).find("a")
+        channel_name = channel_element.text
+        channel_link = channel_element.attrs['href']
+        
+        # print the info to the screen
+        print("Title: " + title)
+        print("Video URL: " + link)
+        print("Views: " + views)
+        print("Posted Time: " + posted_time)
+        print("Channel Name: " + channel_name)
+        print("Channel URL: " + channel_link)
+    pass
